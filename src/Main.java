a233f87cd49dc84f045049e6f5c94ab3b55dc066
Vasiliy Solovev <vasya.79@bk.ru>
2022-12-09 00:24:33 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main (String[] args){
+        List<Person> people = new ArrayList<>();
+        int max=2;
+        PersonComparator personComparator = new PersonComparator(max);
+        people.add(new Person("Vasiliy", "Efipov-Solov'ev-Severniy", 40));
+        people.add(new Person("Nikolay", "Petrov-Zadunaiskiy", 45));
+        people.add(new Person("Yan", "Ivanov", 30));
+        people.add(new Person("Val", "Nosov-Perviy", 15));
+        System.out.println(people);
+        Collections.sort(people, new PersonComparator(max));
+        System.out.println(people);
+
+
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,49 @@
+public class Person implements Comparable<Person> {
+    private String name;
+    private String surname;
+    private int age;
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+
+
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        return surname.compareTo(o.surname);
+
+    }
+
+    @Override
+    public String toString() {
+        return "(" + name + " " + surname + "," + " " + age + "лет) ";
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
