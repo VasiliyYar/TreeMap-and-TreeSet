73cdd3ec00e4cf65f92c409bf9896f988842ded2
Vasiliy Solovev <vasya.79@bk.ru>
2022-12-12 22:02:05 +0300

    remove out

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Main {
     public static void main (String[] args){
         List<Person> people = new ArrayList<>();
-        int max=2;
+        int max=3;
         PersonComparator personComparator = new PersonComparator(max);
         people.add(new Person("Vasiliy", "Efipov-Solov'ev-Severniy", 40));
         people.add(new Person("Nikolay", "Petrov-Zadunaiskiy", 45));
