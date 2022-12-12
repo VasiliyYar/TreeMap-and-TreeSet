4cbc70d7cfe750643513b1ecdd3d904284fc0018
Vasiliy Solovev <vasya.79@bk.ru>
2022-12-13 01:05:05 +0300

    add Lambda

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,40 @@
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.Comparator;
 
 public class Main {
     public static void main (String[] args){
         List<Person> people = new ArrayList<>();
-        int max=3;
-        PersonComparator personComparator = new PersonComparator(max);
+
+        int maxWords=3;
+        //PersonComparator personComparator = new PersonComparator(max);
+        Comparator<Person> personComparator = (o1, o2) -> {
+            int words1 = o1.getSurname().split("-").length;
+            int words2 = o2.getSurname().split("-").length;
+            if (words1 != words2) {
+                if (words1 < maxWords || words2 < maxWords) {
+                    return Integer.compare(words1, words2);
+                } else if (words1 >= maxWords && words2 >= maxWords) {
+                    return Integer.compare(o1.getAge(), o2.getAge());
+                } else {
+                    if (words1 < maxWords || words2 < maxWords) {
+                        return Integer.compare(words1, words2);
+                    } else if (words1 >= maxWords && words2 >= maxWords) {
+                        return Integer.compare(o1.getAge(), o2.getAge());
+                    }
+                }
+            }
+            return Integer.compare(o1.getAge(), o2.getAge());
+
+        };
+
+
         people.add(new Person("Vasiliy", "Efipov-Solov'ev-Severniy", 40));
         people.add(new Person("Nikolay", "Petrov-Zadunaiskiy", 45));
         people.add(new Person("Yan", "Ivanov", 30));
         people.add(new Person("Val", "Nosov-Perviy", 15));
         people.add(new Person("Ilona", "Solov'eva", 17));
         System.out.println(people);
-        Collections.sort(people, new PersonComparator(max));
+        Collections.sort(people, personComparator);
         System.out.println(people);
 
 
